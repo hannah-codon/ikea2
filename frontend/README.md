763c0e57976ef61d68bcf380653e7088527ef735
Elias Carlsson <elias@codon.se>
2025-09-12 14:09:53 +0200

    add ccl

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,16 @@
+# IKEA2 Demo Frontend
 
 ## Requirements
 Node v20.19.4
 npm 10.8.2
+
+## .env
+```
+VITE_API_URL=
+PORT=
+CCL_NPM_TOKEN=
+```
+
+## Commands
+- run the app: `make run`
+- install dependencies `make install-dependencies`
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { CodonButton } from "@codongit/codon-component-library";
+import "@codongit/codon-component-library/styles.css";
+import "./index.css";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <CodonButton onClick={() => console.log("click")}>Click me</CodonButton>
+    </>
+  );
 }
 
 export default App;
