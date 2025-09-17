8fda930563c09f3cdfeb9489ab9539b46b46f715
lidia9085103 <lidia@codon.se>
2025-09-17 14:22:19 +0200

    add comparison function

--- backend/db/crud.py
+++ backend/db/crud.py
@@ -1,6 +1,9 @@
 from db.db import Item, SessionLocal
 from db.models import ItemRead
-
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_item(name: str, embedding: list):
     session = SessionLocal()
@@ -54,3 +57,42 @@ def get_all_items():
     session = SessionLocal()
     items = session.query(Item).all()
     return [ItemRead.model_validate(i) for i in items]
+
+def compare_items(pids_info: list[str])-> str:
+    client = OpenAI(
+            api_key= os.getenv("OPENROUTER_API_KEY"),
+            base_url="https://openrouter.ai/api/v1"
+            )
+    system_prompt_content = """
+    I will give you information about two Ikea chairs and I want
+    you to give me an explanation in two sentences why the chair with 
+    the better score is actually better based on the other information.
+    The information I give you about the chairs are:
+    - score
+    - materials
+    - material score
+    - durability 
+    - weight
+    - weight score
+    This should give some transperency why one chair got a better score.
+    """
+    system_prompt = {
+        "role": "system",
+        "content": system_prompt_content
+    }
+    user_message = {
+        "role": "user",
+        "content":pids_info
+    }
+    messages = [
+        system_prompt,
+        user_message
+    ]
+
+    response = client.chat.completions.create(
+        model="gpt-5",  
+        messages=messages
+    )
+    explanation= response.choices[0].message.content
+
+    return explanation
