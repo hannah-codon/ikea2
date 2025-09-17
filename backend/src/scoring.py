ee407ae26549155e0c7cf4ce38c5a6fabe85fec0
Hannah Klingberg <hannah@codon.se>
2025-09-17 14:36:27 +0200

    suggestion of description generation

--- backend/src/scoring.py
+++ backend/src/scoring.py
@@ -1,13 +1,45 @@
+import os
+
 import pandas as pd
+from openai import OpenAI
 
 material_df_path = "data/materials_ranking.csv"
 material_df = pd.read_csv(material_df_path)
+
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1"
+)
 
 
 def process_df(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
     df = df.apply(process_item, axis=1)
+    df = calc_rel_score(df)
     return df
+
+
+def calc_rel_score(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    num_items = len(df)
+    items_in_split = int(num_items / 3)
+    sorted = df.sort_values(by="score", ascending=False).reset_index(drop=True)
+    sorted["rel_score"] = 0
+    sorted.loc[: items_in_split - 1, "rel_score"] = 1
+    sorted.loc[items_in_split : num_items - 1, "rel_score"] = 2
+    return sorted
+
+
+def gen_description(item: pd.Series) -> str:
+    system_prompt = """Given this description of a furniture item, give me a short description of it (2-3 sentences), 
+    describing its sustainability. The item is scored either 0, 1 or 2, where 0 is the best and 2 is the worst. You will also get some sustainability attributes such as materials, weight and standards, which you should use in your description."""
+    user_prompt = f"""
+       Description: 
+
+       score: {item['rel_score']}
+         Materials: {item['Materials']}
+            Weight: {item['weight']}
+            Standards: {item['standards']}
+         Score calculation: {item['calculation_string']}"""
 
 
 def process_item(item: pd.Series) -> pd.Series:
